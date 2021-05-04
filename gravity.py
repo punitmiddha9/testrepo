d4736b5b2adcad62e2a54af921acbf60be085f0e
punitmiddha9 <70194627+punitmiddha9@users.noreply.github.com>
2021-05-04 20:49:29 +0530

    Add files via upload

---
+++ gravity.py
@@ -0,0 +1,8 @@
+mass=float(input("enter the mass of person"))
+print("the mass of the person is ",mass)
+gra_e=9.8
+print("gravity on earth is",gra_e)
+gra_j=23.1
+print("gravity on jupitar is ",gra_j)
+print("weight on earth=",mass*gra_e)
+print("weight on jupiter=",mass*gra_j)
